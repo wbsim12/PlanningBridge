1f9aaf893eaeb988e0fcb02d8f266a9e46929f57
taeyeon <taenytaeni@gmail.com>
2024-09-12 15:00:51 +0900

    initial commit

---
+++ src/main/java/com/example/planningBridge/PlanningBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.planningBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningBridgeApplication.class, args);
+	}
+
+}
