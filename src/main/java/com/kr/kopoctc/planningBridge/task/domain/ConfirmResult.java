94daa95fdb982d2f6734fed22b4564cc26aa71a0
DESKTOP-24DAOTT\JOOSE <injoy0212@naver.com>
2024-09-16 16:41:15 +0900

    * ENUM 클래스 추가, TaskDomain 작성 * - common 공용 패키지 추가 및 enum 클래스 추가. - TaskDomain Draft 작성

---
+++ src/main/java/com/kr/kopoctc/planningBridge/task/domain/ConfirmResult.java
@@ -0,0 +1,33 @@
+package com.kr.kopoctc.planningBridge.task.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.kr.kopoctc.planningBridge.common.ConfirmStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Getter
+@Setter
+public class ConfirmResult {
+    @Id
+    private String ConfirmResultPK;
+
+    private ConfirmStatus status;
+    private String date;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "ConfirmRequestPK")
+    @JsonBackReference
+    private ConfirmRequest confirmRequest;
+
+  /*  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "projectTeamPK")
+    @JsonBackReference
+    private ProjectTeam projectTeam;*/
+
+
+}
