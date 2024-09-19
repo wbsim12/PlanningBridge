011fe1d9ad5cf6d7f70bb358f16f7ce3f985a705
wbsim12 <simjaewoo12@naver.com>
2024-09-19 16:20:47 +0900

    #재우 ScheduleRepositoryImpl 생성 - ScheduleRepository에 대한 Impl 생성

--- src/main/java/com/kr/kopoctc/planningBridge/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/kr/kopoctc/planningBridge/schedule/repository/ScheduleRepository.java
@@ -1,7 +1,6 @@
 package com.kr.kopoctc.planningBridge.schedule.repository;
 
 import com.kr.kopoctc.planningBridge.schedule.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ScheduleRepository extends CrudRepository<Schedule, String> {
