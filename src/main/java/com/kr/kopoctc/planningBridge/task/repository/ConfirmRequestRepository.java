2bf48c6e62c5ae45361be5e60b683923345cfdd4
DESKTOP-24DAOTT\JOOSE <injoy0212@naver.com>
2024-09-19 09:50:19 +0900

    * DTO, Repository 추가 *

---
+++ src/main/java/com/kr/kopoctc/planningBridge/task/repository/ConfirmRequestRepository.java
@@ -0,0 +1,7 @@
+package com.kr.kopoctc.planningBridge.task.repository;
+
+import com.kr.kopoctc.planningBridge.task.entity.ConfirmRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConfirmRequestRepository extends JpaRepository<ConfirmRequest, String> {
+}
