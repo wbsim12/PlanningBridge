190fe6c2fdb560833da2eeaa5e8c167c149fc5f1
DESKTOP-24DAOTT\JOOSE <injoy0212@naver.com>
2024-09-24 21:44:01 +0900

    * Task Service 구현 및 테스트 중 *

--- src/main/java/com/kr/kopoctc/planningBridge/task/repository/ConfirmRequestRepository.java
+++ src/main/java/com/kr/kopoctc/planningBridge/task/repository/ConfirmRequestRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConfirmRequestRepository extends JpaRepository<ConfirmRequest, String> {
+public interface ConfirmRequestRepository extends JpaRepository<ConfirmRequest, Long> {
 }
--- src/test/java/com/kr/kopoctc/planningBridge/task/service/TaskServiceImplTest.java
+++ src/test/java/com/kr/kopoctc/planningBridge/task/service/TaskServiceImplTest.java
@@ -1,42 +1,77 @@
 package com.kr.kopoctc.planningBridge.task.service;
 
+import com.kr.kopoctc.planningBridge.admin.dto.UserDTO;
+import com.kr.kopoctc.planningBridge.admin.entity.User;
+import com.kr.kopoctc.planningBridge.admin.repository.UserRepository;
+import com.kr.kopoctc.planningBridge.admin.service.UserService;
 import com.kr.kopoctc.planningBridge.task.entity.Task;
 import com.kr.kopoctc.planningBridge.task.repository.TaskRepository;
+import com.kr.kopoctc.planningBridge.task.repository.TaskRepositoryCustom;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
+@Transactional
 class TaskServiceImplTest {
 
-    @InjectMocks
-    private TaskServiceImpl taskService; // 테스트할 클래스
+    @Autowired
+    private TaskServiceImpl taskService;
 
-    @Mock
-    private TaskRepository taskRepository; // 의존하는 레포지토리
+    @Autowired
+    private UserRepository userRepository;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this); // Mockito 초기화
-    }
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskRepositoryCustom taskRepositoryCustom;
+
 
 
     @Test
-    void getProjectPKByUser() {
+    void getUserByUserPK() {
+        // When
+        User user = taskService.getUserByUserPK(1L);
+        System.out.println();
+        // Then
+        assertNotNull(user);
+        assertEquals(1L, user.getUserPK());
+    }
 
+    @Test
+    void getProjectPKByUser() {
+        User user = taskService.getUserByUserPK(1L);
+        List<Long> projectPKList = taskService.getProjectListPKByUser(user);
+        System.out.println("======================");
+        System.out.println(projectPKList.toString());
     }
 
     @Test
     void getTaskByProject() {
-        List<Task> result = taskService.getTaskByProject("1");
-        System.out.println("=====================");
-        System.out.println(result);
-
+        User user = taskService.getUserByUserPK(1L);
+        List<Long> projectPKList = taskService.getProjectListPKByUser(user);
+        List<Task> taskList = taskRepository.findAllById(projectPKList);
+        System.out.println(taskList.toString());
     }
 
     @Test
