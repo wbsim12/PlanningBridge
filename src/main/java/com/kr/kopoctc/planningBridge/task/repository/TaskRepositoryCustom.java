4ddffea1c0d83d48c83bfc6c9cd8f704970cb4e6
DESKTOP-24DAOTT\JOOSE <injoy0212@naver.com>
2024-09-25 11:07:58 +0900

    * 태스크 수정, 테스트 코드 수정 *
    - 태스크 부모 - 자식 관계 무한 재귀호출 해결
    
    - 테스트 코드 간결화 및 오류 해결

--- src/main/java/com/kr/kopoctc/planningBridge/task/repository/TaskRepositoryCustom.java
+++ src/main/java/com/kr/kopoctc/planningBridge/task/repository/TaskRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.kr.kopoctc.planningBridge.task.repository;
 
 import com.kr.kopoctc.planningBridge.admin.entity.User;
+import com.kr.kopoctc.planningBridge.task.entity.Task;
 
 import java.util.List;
 
 public interface TaskRepositoryCustom {
     List<Long> getProjectPKByUser(User user);
+    List<Task> getTaskListByProjectPKList(List<Long> projectPKList);
 }
